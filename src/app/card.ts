581ba6c2cb78c8b53996d84bbd7e64fd07246a8e
Arnaud Drain <drain.arnaud@hotmail.fr>
2019-10-08 23:29:14 +0200

    Show card suit

--- src/app/card.ts
+++ src/app/card.ts
@@ -1,7 +1,48 @@
+const suits: Suit[] = [
+  {
+    symbol: '♠',
+    code: 'spade',
+    color: 'black'
+  },
+  {
+    symbol: '♥',
+    code: 'heart',
+    color: 'red'
+  },
+  {
+    symbol: '♦',
+    code: 'diamond',
+    color: 'red'
+  },
+  {
+    symbol: '♣',
+    code: 'club',
+    color: 'black'
+  },
+];
+
+export class Suit {
+  symbol: string;
+  code: string;
+  color: string;
+}
+
 export class Card {
   value = 0;
   hidden = true;
   dragging = false;
   x = 0;
   y = 0;
+
+  public get cardValue(): string {
+    const numericValue = this.value % 13;
+    if (numericValue > 9) {
+      return ['j', 'q', 'k'][numericValue - 10];
+    }
+    return (numericValue + 1).toString();
+  }
+
+  public get suit(): Suit {
+    return suits[Math.floor(this.value / 13)];
+  }
 }
--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -14,6 +14,7 @@ export class CardComponent {
   @HostBinding('class.dragging') get dragging() { return this.card.dragging; }
   @HostBinding('style.left.px') get left() { return this.card.x; }
   @HostBinding('style.top.px') get top() { return this.card.y; }
+  @HostBinding('style.color') get color() { return this.card.suit.color; }
 
   @HostListener('mousedown', ['$event'])
   onMouseDown(event: any) {
